88efb6c9f0a400c6ee611e76a9ebf8e83bfcfad2
muradv036-stack <muradv036@gmail.com>
2026-03-09 19:38:03 +0300

    Emergency fix: Completely remove Service Worker and PWA setup to restore site availability after ERR_FAILED errors

--- static/sw.js
+++ static/sw.js
@@ -1,23 +1,4 @@
-const CACHE_NAME = 'nonvoyhona-v5';
-const ASSETS = [
-  '/',
-  '/static/css/apple-style.css',
-  '/static/img/logo.png',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      // Just try to cache, don't worry if it fails
-      return Promise.allSettled(ASSETS.map(url => 
-        fetch(url).then(response => {
-           if (response.ok) return cache.put(url, response);
-        }).catch(() => {})
-      ));
-    })
-  );
   self.skipWaiting();
 });
 
@@ -25,13 +6,13 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
+        cacheNames.map((name) => caches.delete(name))
       );
     })
   );
-  return self.clients.claim();
+  self.registration.unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach(client => client.navigate(client.url));
+    });
 });
-
-// Disabled fetch for now to prevent "No Internet" errors on Safari
-// self.addEventListener('fetch', (event) => { ... });
